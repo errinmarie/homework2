240a5386b703d6e3aa52b354bbc647b9f2f5bb6d
Errin Mixon <errinmixon@gmail.com>
2018-09-18 23:00:09 -0700

    ran python script to create combined files; manually moved to Docs folder

--- build.py
+++ build.py
@@ -1,29 +1,55 @@
 about_top_template = open('templates/about_top.html').read()
-
 about_content = open('content/about_index.html').read()
+about1_html = about_top_template + about_content
+open('about1.html', 'w+').write(about1_html)
 
-about_html = about_top_template + about_content
-open('docs/about.html', 'w+').write(about_html)
+contact_top_template = open('templates/contact_top.html').read()
+contact_content = open('content/contact_index.html').read()
+contact1_html = contact_top_template + contact_content
+open('contact1.html', 'w+').write(contact1_html)
 
+belgium_top_template = open('templates/belgium_top.html').read()
+belgium_content = open('content/belgium_index.html').read()
+belgium1_html = belgium_top_template + belgium_content
+open('belgium1.html', 'w+').write(belgium1_html)
 
-#cat templates/about_top.html content/about_index.html > docs/about_index.html
+bhutan_top_template = open('templates/bhutan_top.html').read()
+bhutan_content = open('content/bhutan_index.html').read()
+bhutan1_html = bhutan_top_template + bhutan_content
+open('bhutan1.html', 'w+').write(bhutan1_html)
 
-#cat templates/contact_top.html content/contact_index.html > docs/contact_index.html
+egypt_top_template = open('templates/egypt_top.html').read()
+egypt_content = open('content/egypt_index.html').read()
+egypt1_html = egypt_top_template + egypt_content
+open('egypt1.html', 'w+').write(egypt1_html)
 
-#cat templates/belgium_top.html content/belgium_index.html > docs/belgium_index.html
+india_top_template = open('templates/india_top.html').read()
+india_content = open('content/india_index.html').read()
+india1_html = india_top_template + india_content
+open('india1.html', 'w+').write(india1_html)
 
-#cat templates/bhutan_top.html content/bhutan_index.html > docs/bhutan_index.html
+ireland_top_template = open('templates/ireland_top.html').read()
+ireland_content = open('content/ireland_index.html').read()
+ireland1_html = ireland_top_template + ireland_content
+open('ireland1.html', 'w+').write(ireland1_html)
 
-#cat templates/egypt_top.html content/egypt_index.html > docs/egypt_index.html
+slovenia_top_template = open('templates/slovenia_top.html').read()
+slovenia_content = open('content/slovenia_index.html').read()
+slovenia1_html = slovenia_top_template + slovenia_content
+open('slovenia1.html', 'w+').write(slovenia1_html)
 
-#cat templates/india_top.html content/india_index.html > docs/india_index.html
+spain_top_template = open('templates/spain_top.html').read()
+spain_content = open('content/spain_index.html').read()
+spain1_html = spain_top_template + spain_content
+open('spain1.html', 'w+').write(spain1_html)
 
-#cat templates/ireland_top.html content/ireland_index.html > docs/ireland_index.html
+uzbekistan_top_template = open('templates/uzbekistan_top.html').read()
+uzbekistan_content = open('content/uzbekistan_index.html').read()
+uzbekistan1_html = uzbekistan_top_template + uzbekistan_content
+open('uzbekistan1.html', 'w+').write(uzbekistan1_html)
 
-#cat templates/slovenia_top.html content/slovenia_index.html > docs/slovenia_index.html
+vietnam_top_template = open('templates/vietnam_top.html').read()
+vietnam_content = open('content/vietnam_index.html').read()
+vietnam1_html = vietnam_top_template + vietnam_content
+open('vietnam1.html', 'w+').write(vietnam1_html)
 
-#cat templates/spain_top.html content/spain_index.html > docs/spain_index.html
-
-#cat templates/uzbekistan_top.html content/uzbekistan_index.html > docs/uzbekistan_index.html
-
-#cat templates/vietnam_top.html content/vietnam_index.html > docs/vietnam_index.html
